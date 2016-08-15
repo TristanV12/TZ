44798a0c92f61e6831ed81554bb05e8e1df8bddd
TristanV12 <villamil101@gmail.com>
2016-08-15 13:35:08 -0400

    Add index

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var app      = require('express')();
+var express  = require('express');
+var app      = express();
 var http     = require('http').Server(app);
 var mongoose = require('mongoose');
 var io       = require('socket.io')(http);
@@ -7,25 +8,44 @@ mongoose.connect('mongodb://localhost/TZ/mongo');
 //populate database with cards if it is not already populated
 var card = mongoose.model('card', { id: Number, value: Number, suit: Number}); //card variable
 
-card.findOne({}, function( err, c=null ){
-	if(c == null){
+// card.remove({}, function(err) { 
+//    console.log('collection removed') 
+// });
+
+card.findOne({}, function( err, c ){
+	if(c == null){ //check if cards have been initialized
+		console.log("Running setup...");
+		//loop to add cards
+		//	2-10 numbered
+		//	11 Jack
+		//	12 Queen
+		//	13 King
+		//	14 Ace
 		for (var i = 0; i < 52; ++i) {
 			var new_card = new card({ id: i, value: (i % 13) + 2, suit: i % 4 });
 			new_card.save();
 		};
+		//add Jokers
+		//	15 Jokers
+		//	Suit 4
 		var new_card = new card({ id: 52, value: 15, suit: 4 });
 		new_card.save();
 		var new_card = new card({ id: 53, value: 15, suit: 4 });
 		new_card.save();
+		console.log("Setup complete");
+	}else{
+		console.log("Cards already loaded.");
 	}
 });
-card.find({}, function(err, users){
-	console.log(users)
-});
+// card.find({}, function(err, users){
+// 	console.log(users)
+// });
 
+// set the static files location (ex. /public/img will be /img for users)
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
-	res.send('<h1>Hello world</h1>');
+	res.sendFile('index.html');
 });
 
 http.listen(3000, function(){
