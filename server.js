e0a6e60cb7b9f0bbbdd3dbcc13b2969d27221c5d
TristanV12 <villamil101@gmail.com>
2016-08-15 11:22:07 -0400

    Update server and gitignore

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var app      = require('express')();
+var http     = require('http').Server(app);
+var mongoose = require('mongoose');
+var io       = require('socket.io')(http);
+mongoose.connect('mongodb://localhost/TZ/mongo');
 
 app.get('/', function(req, res){
 	res.send('<h1>Hello world</h1>');
 });
 
 http.listen(3000, function(){
+	console.log('TZ now running');
 	console.log('listening on *:3000');
 });
